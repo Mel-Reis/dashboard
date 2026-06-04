addfb4009d12237e3d44ce76bd6d995f641d1918
Mel Reis <melvivireis79@gmail.com>
2026-06-04 18:26:47 -0300

    att

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import logo from "../../assets/logo.jpg";;
 
 function Sidebar() {
   return (
     /* Aqui você pode adicionar os itens do seu sidebar */
 
     <div className="transition duration-300 ease-in-out bg-white/80 dark:bg-gray-800/80 backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-600/40 flex flex-col relative z-10">
-      <div className="">
-
-
-        
+      {/* logo */}
+      <div className="p-4 border-b border-slate-200/50 dark:border-slate-600/40">
+          <div className="flex items-center space-x-3">
+             <div>
+               <img src={logo} alt="Logo" className="w-8 h-8 rounded-xl" />
+             </div>
+          </div>
+       
       </div>
       
     </div>
