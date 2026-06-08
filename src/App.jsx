6f5505df11ce870441855a51c42c735acda43d23
Mel-Reis <melvivireis79@gmail.com>
2026-06-08 16:36:31 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Sidebar from './components/Layout/Sidebar';
 import Header from './components/Layout/Header';
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -7,14 +7,14 @@ function Header() {
     <div className='bg-white/80 dark:bg-gray-800/80 backdrop-blur-xl border-b border-slate-200/50 dark:border-slate-600/40 px-4 py-4'>
       
       {/* Aqui você pode adicionar os itens do seu header */}
-      <div className=" p-1 flex items-center justify-between">
+      <div className="  flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <button className="p-2 text-gray-500 dark:text-gray-400 focus:outline-none hover:bg-gray-500 dark:hover:bg-gray-700 transition-colors ">
            <Menu className="h-6 w-6" />
           </button>
 
         <div className="hidden md:block">
-          <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+          <h1 className="text-xl font-bold text-gray-800 dark:text-gray-200">
             Dashboard
           </h1>
 
