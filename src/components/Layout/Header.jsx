760bdb853cdee9341fc7f56a923ae2c120a8873b
Mel-Reis <melvivireis79@gmail.com>
2026-06-08 21:21:11 -0300

    nav

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Filter, Menu, Search } from 'lucide-react';
+import { Filter, Menu, Plus, Search, Settings, Sun } from 'lucide-react';
 
 function Header() {
   return (
     <div className='bg-white/80 dark:bg-gray-800/80 backdrop-blur-xl border-b border-slate-200/50 dark:border-slate-600/40 px-4 py-4'>
       
-      {/* Aqui você pode adicionar os itens do seu header */}
+      {/* Menu */}
       <div className="  flex items-center justify-between">
         <div className="flex items-center space-x-4">
           <button className="p-2 text-gray-500 dark:text-gray-400 focus:outline-none hover:bg-gray-500 dark:hover:bg-gray-700 transition-colors ">
            <Menu className="h-6 w-6" />
           </button>
-
+        {/* Header info */}
         <div className="hidden md:block">
           <h1 className="text-xl font-bold text-gray-800 dark:text-gray-200">
             Dashboard
@@ -33,11 +33,26 @@ function Header() {
               placeholder="Search..."
               className="w-full pl-10 pr-4 py-2 rounded-xl bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:bg-gray-300 dark:focus:bg-gray-600  focus:border-transparent transition-all"
             />
-           {/* Depois ajeito o hover */}`
-            <button className=" absolute right-2 top-1/2 transform -translate-y-1/2 py-1.5 bg-gray-700 dark:bg-gray-750 rounded-3xl hover:bg-gray-700 dark:hover:bg-gray-600">
+         
+            <button className=" absolute right-2 top-1/2 transform -translate-y-1/2 py-1.5 bg-gray-700 dark:bg-gray-750 rounded-3xl hover:bg-gray-600 dark:hover:bg-gray-650 transition-colors px-3">
               <Filter className="w-4 h-4 text-gray-500 dark:text-gray-400" />
             </button>
           </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <button className="hidden lg:flex w-8 h-8 rounded-full bg-gray-300 dark:bg-gray-600  items-center justify-center text-sm font-medium text-gray-800 dark:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-500 transition-colors">
+            <Plus className="w-4 h-4" />
+            <span className="sr-only">Novo</span>
+          </button>
+
+          <button className="p-2 text-gray-500 dark:text-gray-400 focus:outline-none hover:bg-gray-500 dark:hover:bg-gray-700 transition-colors rounded-xl">
+            <Sun className="w-4 h-4" />
+          </button>
+
+          <button className="p-2 text-gray-500 dark:text-gray-400 focus:outline-none hover:bg-gray-500 dark:hover:bg-gray-700 transition-colors rounded-xl">
+            <Settings className="w-4 h-4" />
+          </button>
+
         </div>
       </div>
 
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,6 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import logo from "../../assets/logo.jpg";
+import {
+  LayoutDashboard,
+  Users,
+  Settings,
+  FileText
+} from 'lucide-react';
+
+const menuItems = [
+  {id: "dasboard",
+   icon: LayoutDashboard,
+   label: "Dashboard",
+   active: true,
+   badge:"New"
+  },
+  {
+    id: "users",
+    icon: Users,
+    label: "Users",
+    active: false
+  },
+  {
+    id: "settings",
+    icon: Settings,
+    label: "Settings",
+    active: false
+  },
+  {
+    id: "documents",
+    icon: FileText,
+    label: "Documents",
+    active: false
+  }
+];
 
 
 function Sidebar() {
@@ -30,7 +63,22 @@ function Sidebar() {
 
 
       {/* itens do sidebar - Navigation*/}
-      <nav className="flex-1 p-4 space-y-2 overflow-y-auto"></nav>
+      <nav className="flex-1 p-4 space-y-2 overflow-y-auto">
+  {menuItems.map((item) => {
+    const Icon = item.icon;
+
+    return (
+      <div key={item.id}>
+        <button className="flex items-center w-full p-2 rounded-xl transition-all duration-200 justify-between">
+          <div className="flex items-center space-x-3">
+            <Icon className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+            <span className="text-gray-700 dark:text-gray-300">{item.label}</span>
+          </div>
+        </button>
+      </div>
+    );
+  })}
+</nav>
 
      <div className="p-4 border-t border-slate-200/50 dark:border-slate-600/40">
         <div className="flex items-center justify-center">
