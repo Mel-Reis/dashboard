c610720ad25a6c249e2f57e3f25e973c54ce8fae
Mel-Reis <melvivireis79@gmail.com>
2026-06-09 14:17:32 -0300

    init-main

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,15 @@ function App() {
             onPageChange={setCurrentPage}
           />
           <div className="flex-1 flex flex-col overflow-hidden">
-            
+
             {/* Header */}
             <Header />
-            
+            <main className="flex-1 p-6 overflow-y-auto">
+              {/* Conteúdo principal do dashboard */}
+              <h2 className="text-2xl font-semibold text-gray-800 dark:text-gray-200 mb-4">
+                O conteúdo principal do dashboard vai aqui. 
+              </h2>
+            </main>
           </div>
         </div>
 
